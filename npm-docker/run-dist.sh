140392f358713aeab7adb9048445e562a14b616e
marc-p-b <marc@b7s.me>
2025-11-29 11:29:24 +0100

    new docker build script

--- npm-docker/run-dist.sh
+++ npm-docker/run-dist.sh
@@ -5,7 +5,7 @@ if [ -f ./ui/env.js ];then
   mv ./ui/env.js /tmp/env.js && saved=1
 fi
 cp ./ui/index_dist.html ./ui/index.html
-docker run -it --rm -p 5173:5173 -v ./ui/:/ui npm-ui bash /dist.sh
+docker run -it --rm -u $(id -u):$(id -g) -p 5173:5173 -v ./ui/:/ui npm-ui bash /dist.sh
 if [ $saved = 1 ];then
   echo ">> Restoring env.js"
   mv /tmp/env.js ./ui/env.js
--- npm-docker/run-serve.sh
+++ npm-docker/run-serve.sh
@@ -1,6 +1,6 @@
 if [ -f ./ui/env.js ];then
   cp ./ui/index_serve.html ./ui/index.html
-  docker run -it --rm -p 5173:5173 -v ./ui/:/ui npm-ui
+  docker run -it --rm -u $(id -u):$(id -g) -p 5173:5173 -v ./ui/:/ui npm-ui
 else
   echo "--- ERROR ---"
   echo "Please provide /ui/env.js"
